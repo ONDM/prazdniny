355afea19c3eae21cf7bf5af90d0cc896bab5c76
OND <149911739+ONDM@users.noreply.github.com>
2024-01-22 10:04:35 +0100

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,7 @@
 // SERVICE-WORKER STATUS
 if ('serviceWorker' in navigator)
 {
-  navigator.serviceWorker.register('/service-worker.js')
+  navigator.serviceWorker.register('/prazdniny/service-worker.js')
   .then((registration) =>
   {
     console.log('Service Worker zaregistrován:', registration);
