e348f74539f8ec6f7dbf0ffc44fc29ce551df4d7
OND <149911739+ONDM@users.noreply.github.com>
2024-01-22 14:54:28 +0100

    Update script.js

--- script.js
+++ script.js
@@ -80,3 +80,82 @@ function hideInfo()
 {
   document.getElementById('info-overlay').style.display = 'none';
 }
+
+
+// PADÁNÍ VLOČEK
+document.addEventListener("DOMContentLoaded", function ()
+{
+  const snowflakesContainer = document.getElementById("snowflakes-container");
+  const currentDate = new Date();
+  const startDate = new Date(currentDate.getFullYear(), 0, 20);    // datum se indexuje od 0 po 11, takže 0 = leden a 11 = prosinec
+  const endDate = new Date(currentDate.getFullYear(), 0, 30);    // datum se indexuje od 0 po 11, takže 0 = leden a 11 = prosinec
+
+  if (currentDate >= startDate && currentDate < endDate)
+  {
+    const numberOfSnowflakes = 500;
+    const snowflakes = [];
+
+    function random(min, max)
+    {
+      return Math.random() * (max - min) + min;
+    }
+
+    class Snowflake
+    {
+      constructor()
+      {
+        this.element = document.createElement("div");
+        this.element.className = "snowflake";
+        this.element.innerHTML = "*";
+        snowflakesContainer.appendChild(this.element);
+
+        this.x = random(0, window.innerWidth);
+        this.y = 0;
+        this.size = random(5, 18);
+        this.speedX = random(-0.5, 0.5);
+        this.speedY = random(0.5, 1.5);
+      }
+
+      update()
+      {
+        this.y += this.speedY;
+        this.x += this.speedX;
+
+        if (this.y > window.innerHeight)
+        {
+          this.y = 0;
+          this.x = random(0, window.innerWidth);
+        }
+
+        this.element.style.transform = `translate(${this.x}px, ${this.y}px)`;
+        this.element.style.fontSize = `${this.size}px`;
+      }
+    }
+
+    function createSnowflake()
+    {
+      snowflakes.push(new Snowflake());
+    }
+
+    function createSnowflakeGroup(groupSize, delay)
+    {
+      for (let i = 0; i < groupSize; i++)
+      {
+        setTimeout(createSnowflake, i * delay);
+      }
+    }
+
+    function moveSnowflakes()
+    {
+      for (let i = 0; i < snowflakes.length; i++)
+      {
+        snowflakes[i].update();
+      }
+
+      requestAnimationFrame(moveSnowflakes);
+    }
+
+    createSnowflakeGroup(numberOfSnowflakes, 200);
+    moveSnowflakes();
+  }
+});
