60d3078494a145ae1eccdb838d697b3a35581c79
OND <149911739+ONDM@users.noreply.github.com>
2024-02-25 16:45:05 +0100

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,29 +1,38 @@
-const cacheName = 'prazdniny-cache-v2';
+const cacheName = 'prazdniny-v1';
+const filesToCache = [
+  '/prazdniny/',
+  '/prazdniny/manifest.json',
+  '/prazdniny/style.css',
+  '/prazdniny/script.js',
+  '/prazdniny/favicon.png',
+  '/prazdniny/font/font1.woff2',
+];
 
-self.addEventListener('install', (event) =>
-{
+self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(cacheName).then((cache) =>
-    {
-      return cache.addAll([
-        '/prazdniny/',
-        '/prazdniny/manifest.json',
-        '/prazdniny/style.css',
-        '/prazdniny/script.js',
-        '/prazdniny/favicon.png',
-        '/prazdniny/font/font1.woff2',
-        '/prazdniny/sw.js',
-      ]);
-    })
+    caches.open(cacheName)
+      .then(cache => cache.addAll(filesToCache))
   );
 });
 
-self.addEventListener('fetch', (event) =>
-{
+self.addEventListener('fetch', event => {
   event.respondWith(
-  caches.match(event.request).then((response) =>
-  {
-    return response || fetch(event.request);
-  })
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys()
+      .then(cacheNames => {
+        return Promise.all(
+          cacheNames.map(existingCacheName => {
+            if (existingCacheName !== cacheName) {
+              return caches.delete(existingCacheName);
+            }
+          })
+        );
+      })
   );
 });
