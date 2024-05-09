db288100fdf08173e2672fce44505dd4be608f11
OND <149911739+ONDM@users.noreply.github.com>
2024-05-09 22:23:01 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,11 +2,10 @@ const cacheName = 'prazdniny-v1';
 const offlinePage = 'offline.html';
 const assets = [
   '/prazdniny/',
-  '/prazdniny/index.html',
   '/prazdniny/style.css',
   '/prazdniny/script.js',
-  '/prazdniny/favicon.png',
-  '/prazdniny/font1.woff2',
+  '/prazdniny/favicon.jpg',
+  '/prazdniny/font/font1.woff2',
   '/prazdniny/manifest.json',
   '/prazdniny/logo.png',
 ];
